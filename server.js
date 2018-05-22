c3aaeb4fd0add7a4746bccbeb015a33f10dbe0d6
Oleg Tserkovnyuk <Oleg.Tserkovnyuk@mccain.ca>
2018-05-22 11:54:49 -0300

    Changed IP

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app            = express();
 const port = 8000;
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
