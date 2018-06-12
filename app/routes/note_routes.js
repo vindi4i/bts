976faf39fd80ab1d513b4dfad1d74ae539fe3284
Oleg Tserkovnyuk <Oleg.Tserkovnyuk@mccain.ca>
2018-06-12 09:32:27 -0300

    Stable version

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,18 +1,24 @@
+var mongodb = require('mongodb');
+
 module.exports = function(app, db) {
 
   var ObjectID = require('mongodb').ObjectID;
 
+  //read data
   app.get('/read', (req, res) => {
     const collection = db.collection('tests');
     collection.find({}).toArray(function(err, docs) {
-      console.log("Found the following records");
-      console.log(docs)
-      //callback(docs);
+      if(err) {
+        res.send({ 'error': 'An error has occurred' });
+      } else {
+        res.send(docs);
+      }
     });
   });
 
+//add tests
   app.post('/register', (req, res) => {
-    const singletest = { id: req.body.id, DeviceName: req.body.DeviceName, MaxRatioError: req.body.MaxRatioError };
+    const singletest = { DeviceName: req.body.DeviceName, MaxRatioError: req.body.MaxRatioError, EventDate: date(), Data: req.body.Data, VectorGroup: req.body.VectorGroup, SoftwareVersion: req.body.SoftwareVersion };
     db.collection('tests').insert(singletest, (err, result) => {
       if (err) {
         res.send({ 'error': 'An error has occurred' });
@@ -21,4 +27,41 @@ module.exports = function(app, db) {
       }
     });
   });
+
+//delete tests
+  app.delete('/delete', (req, res) => {
+  console.log('Type ' + typeof req.body._id);
+  console.log('Content ' + req.body._id);
+  if (typeof req.body._id === 'string') {
+    db.collection('tests').deleteOne({_id:new mongodb.ObjectID (req.body._id)}, function (err, result) {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send('Item deleted!');
+      }
+    });
+  }
+  else {
+    var RecordsToDelete = req.body._id;
+    for (var i = 0; i < RecordsToDelete.length; i++) {
+      db.collection('tests').deleteOne({_id:new mongodb.ObjectID (RecordsToDelete[i])}, function (err, result) {
+        if (err) {
+          res.send({'error':'An error has occurred'});
+        }
+      });
+    }
+    res.send('Item deleted!');
+  }
+});
+
+function date(){
+  var currentdate = new Date();
+  var datetime = currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/"
+                + currentdate.getFullYear() + " @ "
+                + currentdate.getHours() + ":"
+                + currentdate.getMinutes() + ":"
+                + currentdate.getSeconds();
+  return datetime;
+}
 };
--- server.js
+++ server.js
@@ -10,6 +10,12 @@ var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 MongoClient.connect(db.url, (err, database) => {
   //assert.equal(null, err);
